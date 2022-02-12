e63aa5aa2a70052c0152e3e3e740e7e8a948382f
Jim Orluk <jim.orluk@gmail.com>
2022-02-12 16:46:45 -0500

    npm updates and config update

--- svelte.config.js
+++ svelte.config.js
@@ -3,9 +3,8 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
-
-		// hydrate the <div id="svelte"> element in src/app.html
+		adapter: adapter(),
+		target: '#svelte'
 	}
 };
 
